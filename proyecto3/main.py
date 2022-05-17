966ad48a689b5071f364584b9ddc1d99e7a85186
david Corzo <davidcorzo@ufm.edu>
2022-05-17 00:37:42 -0600

    22

--- proyecto3/main.py
+++ proyecto3/main.py
@@ -1,77 +1,54 @@
-from concurrent.futures import ThreadPoolExecutor, thread
-from dataclasses import dataclass
 import threading
-import sqlalchemy as db
-from queue import Queue
-from webscrapping import WebScrapper
-from random import randint
 import time
+import logging
+import random
+import queue
+import webscrapping
 
-# docker exec -it mysql_docker bash
+logging.basicConfig(level=logging.DEBUG, format='(%(threadName)-9s) %(message)s',)
 
-# dbconfig = {
-#     'host':'localhost',
-#     'port':3306,
-#     'user':'admin',
-#     'password':'123',
-#     'database':'test'
-# }
-# connection_url = f"mysql+pymysql://{dbconfig['user']}:{dbconfig['password']}@{dbconfig['host']}:{dbconfig['port']}/{dbconfig['database']}"
-# engine = db.create_engine(connection_url)
-# connection = engine.connect()
+BUF_SIZE = 10
+q = queue.Queue(BUF_SIZE)
+aws = webscrapping.ActorWebScrapper(
+    url="https://en.wikipedia.org/wiki/List_of_American_film_actresses"
+)
 
-# exit()
+class ProducerThread(threading.Thread):
+    def __init__(self, target=None, name=None):
+        super(ProducerThread,self).__init__()
+        self.target = target
+        self.name = name
 
-
-@dataclass
-class actor:
-    name:str
-    url:str
-
-class ConsumerProducer:
-    def __init__(self, buffer_size, url, consumer_threads=4, producer_threads=4):
-        self.q:Queue        = Queue(buffer_size)
-        self.buff_size      = buffer_size
-        self.producer_lock  = threading.Lock()
-        self.consumer_lock  = threading.Lock()
-        self.buff_index     = 0 # shared resource
-        self.consumer_tp    = ThreadPoolExecutor(max_workers=consumer_threads)
-        self.producer_tp    = ThreadPoolExecutor(max_workers=producer_threads)
-    
-    def producer_work(self):
-        
-        pass
-    
-    def consumer_work(self):
-        
-        pass
-    
-    def produce(self):
+    def run(self):
         while True:
-            if not self.q.full():
-                self.producer_tp.submit()
-                # begin: area critica
-                self.producer_lock.acquire()
-                self.buff_index += 1 
-                self.consumer_lock.release()
-                # end: area critica
+            if not q.full():
+                item = next(aws.producer_data)
+                q.put(item)
+                logging.debug(f'Putting {item} : {q.qsize()} items in queue')
+                time.sleep(random.random())
+        return
 
-    def consume(self):
+class ConsumerThread(threading.Thread):
+    def __init__(self, target=None, name=None):
+        super(ConsumerThread,self).__init__()
+        self.target = target
+        self.name = name
+        return
+
+    def run(self):
         while True:
-            if not self.q.empty():
-                item = self.q.get()
-                print(item)
-                time.sleep(1)
-                # begin: area critica
-                self.consumer_lock.acquire()
-                self.buff_index = (self.buff_index + 1) % self.buff_size
-                self.producer_lock.release()
-                # end: area critica
-
-
-NUM_THREADS = 4
+            if not q.empty():
+                title, url = q.get()
+                logging.debug(f'Getting {(title, url)} from queue')
+                # logging.debug('Getting ' + str(item) + ' : ' + str(q.qsize()) + ' items in queue')
+                time.sleep(random.random())
+        return
 
 if __name__ == '__main__':
-    cp = ConsumerProducer(buffer_size=10, url="https://en.wikipedia.org/wiki/List_of_American_film_actresses")
-    
-    pass
+    p = ProducerThread(name='producer')
+    c = ConsumerThread(name='consumer')
+
+    p.start()
+    time.sleep(2)
+    c.start()
+    time.sleep(2)
--- proyecto3/webscrapping.py
+++ proyecto3/webscrapping.py
@@ -1,22 +1,42 @@
+from concurrent.futures import ThreadPoolExecutor
 import bs4
 import requests
 import re
 from typing import Tuple
-from threading import Lock
+from actor_struct import actor
+import threading
+import asyncio
 from queue import Queue
 
-# page = requests.get(URL)
+class safe_set(set):
+    def __init__(self):
+        super(safe_set, self).__init__()
+        self.mutex = threading.Lock()
+    
+    def add_actor(self, item):
+        self.mutex.acquire()
+        self.add(item)
+        self.mutex.release()
+    
+    def pop_actor(self, item):
+        self.mutex.acquire()
+        p = self.pop()
+        self.mutex.release()
+        return p
+
 class ActorWebScrapper:
-    url_prefix = "https://en.wikipedia.org/"
-    def __init__(self, url, buff_size):
+    url_prefix = "https://en.wikipedia.org"
+    def __init__(self, url:str):
         self.url = url
-        self.page = requests.get(url)
-        self.soup = bs4.BeautifulSoup(self.page.content, 'html.parser')
-        self.names_urls = set()
-        self.lock = Lock()
-        self.buffer = Queue(maxsize=buff_size)
+        self.registered_actors = safe_set()
+        self.registered_movies = safe_set()
+        self.movie_unmatches = safe_set()
+        self.registered_movies = safe_set()
+        self.producer_data = self.producer_work()
     
     def producer_work(self) -> Tuple[str, str]:
+        self.page = requests.get(self.url)
+        self.soup = bs4.BeautifulSoup(self.page.content, 'html.parser')
         for div in (self.soup.find_all("div", {"class":'div-col', "style": 'column-width: 25em;'})):
             for ul in div.find_all("ul"):
                 for li in ul.find_all("li"):
@@ -26,34 +46,98 @@ class ActorWebScrapper:
                         if re.match(r"[a-zA-Z\(\)%]+", title):
                             actress_url = None
                             if re.match(r"/wiki/[a-zA-Z_]", url):
-                                actress_url = tuple([title, url.replace("/wiki", "")])
+                                actress_url = url
                             elif re.match(r"/w/index\.php\?title=[a-zA-Z]&action=edit&redlink=1", url):
-                                actress_url = tuple([title, None])
+                                actress_url = None
                             # dont return duplicates.
-                            if actress_url in self.names_urls:
-                                yield actress_url
+                            if (actress_url):
+                                if (actress_url not in self.registered_actors):
+                                    actor_tuple = (title, url)
+                                    self.registered_actors.add_actor(actor_tuple)
+                                    yield actor_tuple
+                                else:
+                                    continue
                             else:
                                 continue
                         else:
-                            print(title, url)
-    
-    def producer_send_work_to_queue(self):
-        pass
+                            print(f"Title and url not found.")
+
+    def cut_html_filmography(self, content, url) -> Tuple[bool, str]:
+        # cuts html content
+        id_film = f"id=\"[a-eg-zA-EG-Z_-]*(((F|f)ilmography)|(Film_and_television_credits))[a-zA-Z_-]*\""
+        class_film = f"class=\"mw-headline\""
+        order_film = ((class_film, id_film), (id_film, class_film))
+        start_filmography = None
+        for o in order_film:
+            start_filmography = re.search(f'<h2>[ \t\n]*<span\s+{o[0]}\s+{o[1]}>', content, re.DOTALL)
+            if start_filmography: break
+        if start_filmography is None: return None
+        id_ref = f"id=\"[a-qs-zA-QS-Z_-]*(R|r)eference[a-zA-Z_-]*\""
+        class_ref = f"class=\"mw-headline\""
+        order_ref = ((id_ref, class_ref), (class_ref, id_ref))
+        end_filmography = None
+        sf = int(start_filmography.start())
+        for o in order_ref:
+            end_filmography = re.search(f'<h2>[ \t]*<span {class_ref} {id_ref}>', content[sf:], re.DOTALL)
+            if end_filmography: break
+        if not end_filmography:
+            return None
+        ef = sf + int(end_filmography.start())
+        # # start string.
+        return content[sf:ef]
     
     def consumer_work(self, actress_url):
         if actress_url:
-            pass
-        else:
-            pass
-        pass
+            if (self.url_prefix.endswith('/')) ^ (actress_url.startswith('/')):
+                url = f"{self.url_prefix}{actress_url}"
+            elif (not self.url_prefix.endswith('/')) and (not actress_url.startswith('/')):
+                url = f"{self.url_prefix}/{actress_url}"
+            elif (self.url_prefix.endswith('/')) and (actress_url.startswith('/')):
+                url = f"{self.url_prefix[:-1]}{actress_url}"
+            else:
+                print("ERROR: url not valid.")
+                return
+            a_req = requests.get(url)
+            content = self.cut_html_filmography(a_req.text, url=actress_url)
+            if content is None:
+                return False
+            soup = bs4.BeautifulSoup(content, 'html.parser')
+            has_tables = bool(soup.find('table'))
+            has_lists = bool(soup.find('ul'))
+            if (has_tables ^ has_lists):
+                if has_tables:
+                    for table in soup.findAll("a"):
+                        title = table.get('title')
+                        href = table.get('href')
+                        if (title and href) and (re.match(r"[a-zA-Z-_()]+", title) and re.match(r"/wiki/[a-zA-Z_]", href)):
+                            self.registered_movies.add((title, href))
+                        else:
+                            self.movie_unmatches.add((title, href))
+                elif has_lists:
+                    for ul in soup.findAll("ul"):
+                        for li in ul.findAll("li"):
+                            for i in li.findAll("i"):
+                                for a in i.findAll("a"):
+                                    title = a.get('title')
+                                    href  = a.get('href')
+                                    if (title and href) and (re.match(r"[a-zA-Z-_()]+", title) and re.match(r"/wiki/[a-zA-Z_]", href)):
+                                        self.registered_movies.add((title, href))
+                                    else:
+                                        self.movie_unmatches.add((title, href))
+            else: # has_tables == has_lists
+                for i in soup.findAll("i"):
+                    for a in i.findAll("a"):
+                        title = a.get('title')
+                        href  = a.get('href')
+                        if (title and href) and (re.match(r"[a-zA-Z-_()]+", title) and re.match(r"/wiki/[a-zA-Z_]", href)):
+                            self.registered_movies.add((title, href))
+                        else:
+                            self.movie_unmatches.add((title, href))
+        return True
     
-    def produce():
 
-        pass
 
-    def consume():
 
-        pass
-    
 
-aws = ActorWebScrapper("https://en.wikipedia.org/wiki/List_of_American_film_actresses", 10)
+
+
